99bd4212440e1a3f51e49f8e93f457cb356c2bc4
Cédric BILLARD <cbillard@com-company.fr>
2025-11-07 17:18:58 +0100

    About section : skills + Education timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/app/components/HeroSection";
 import Navbar from "@/app/components/Navbar";
+import AboutSection from "@/app/components/AboutSection";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
           <Navbar />
           <div className="container mt-4 md:mt-24 mx-auto  px-4 md:px-12 py-4">
             <HeroSection />
+              <AboutSection />
           </div>
       </main>
   );
